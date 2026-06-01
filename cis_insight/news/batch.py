e73daedbbc501b9d5cef119afa07695740e12a5d
Inari Goro <qqnq7um9k@gmail.com>
2026-06-01 21:44:03 +0900

    refactor: optimize db processing

--- cis_insight/news/batch.py
+++ cis_insight/news/batch.py
@@ -1,4 +1,3 @@
-from apscheduler.schedulers.background import BackgroundScheduler
 from .models import NewsRss, NewsArticle, Topic
 import logging
 import feedparser
@@ -9,7 +8,6 @@ from django.db import transaction
 from django.utils import timezone
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.files.uploadedfile import InMemoryUploadedFile
-import undetected_chromedriver as uc
 
 import datetime
 import time
@@ -18,9 +16,7 @@ import random
 import string
 from PIL import Image
 from io import BytesIO
-import newspaper
-from newspaper import Article, Config
-import nltk
+
 import sys
 
 from core.exceptions import RateLimitError
@@ -28,129 +24,122 @@ from core.settings import MAXIMUM_IMAGE_SIZE_PIXEL
 
 logger = logging.getLogger(__name__)
 
-# TODO: DBアクセスを減らして負荷を軽減する
 def fetch_news_articles():
     processed_urls = set()
+    all_topics = list(Topic.objects.all())
 
     for rss in NewsRss.objects.filter(is_active = True):
-        news_articles_count = 0
+        pending_articles = []
 
         try:
             feed = feedparser.parse(rss.url)
-
-            for entry in feed.entries:
-                try:
-                    rss.refresh_from_db()
-                    if rss.is_active == False:
-                        break
-                except ObjectDoesNotExist:
-                    break
-
-                if 'link' not in entry or 'title' not in entry:
-                    continue
-
-                if entry.link in processed_urls:
-                    continue
-                processed_urls.add(entry.link)
-
-                url = entry.link
-
-                if NewsArticle.objects.filter(url = url).exists():
-                    continue
-
-                title_ru = entry.title
-
-                try:
-                    topic = pick_up_news_article_topic(title_ru)
-
-                    if topic is None:
-                        continue
-
-                    title_ja = translate_title(title_ru)
-
-                    if title_ja is None:
-                        continue
-                except RateLimitError:
-                    logger.error(f'Rate limit error while translating topics or title')
-                    rss.last_error = 'Rate limit error while translating topics or title'
-                    rss.save()
-                    return
-                except Exception as e:
-                    logger.error(f'Error translating topics or title: {e}')
-                    rss.last_error = f'Error translating topics or title: {e}'
-                    rss.save()
-                    continue
-
-                if 'published_parsed' in entry:
-                    published_at = timezone.make_aware(datetime.datetime.fromtimestamp(time.mktime(entry.published_parsed)), datetime.timezone.utc)
-                else:
-                    published_at = None
-                
-                if 'enclosures' in entry and len(entry.enclosures) > 0:
-                    image_url = entry.enclosures[0].href
-                elif 'media_content' in entry and len(entry.media_content) > 0:
-                    image_url = entry.media_content[0]['url']
-                elif 'media_thumbnail' in entry and len(entry.media_thumbnail) > 0:
-                    image_url = entry.media_thumbnail[0]['url']
-                else:
-                    image_url = None
-                
-                if image_url is not None:
-                    try:
-                        image = download_image(image_url)
-                    except Exception as e:
-                        logger.error(f'Error downloading image from {image_url}: {e}')
-                        image = None
-                else:
-                    image = None
-                
-                country = rss.country
-
-                is_title_added = True if title_ru is not None else False
-                is_title_translated = True if title_ja is not None else False
-                is_topic_picked = True if topic is not None else False
-
-                if is_title_added and is_title_translated and is_topic_picked:
-                    is_active = True
-                else:
-                    is_active = False
-
-                try:
-                    rss.refresh_from_db()
-                    if rss.is_active == False:
-                        break
-
-                    with transaction.atomic():
-                        news_article, created = NewsArticle.objects.get_or_create(
-                            url = url,
-                            defaults = {
-                                'title_ru': title_ru,
-                                'title_ja': title_ja,
-                                'published_at': published_at,
-                                'country': country,
-                                'image': image,
-                                'url': url,
-                                'rss': rss,
-                                'is_active': is_active,
-                                'is_title_added': is_title_added,
-                                'is_title_translated': is_title_translated,
-                                'is_topic_picked': is_topic_picked,
-                            }
-                        )
-
-                        if created:
-                            news_article.topic.set(topic)
-                            news_article.save()
-                            news_articles_count += 1
-                except Exception as e:
-                    logger.error(f'Error creating news article from {rss.url}: {e}')
-                    rss.last_error = e
-                    rss.save()
-                    continue
             
-            rss.last_fetched_at = timezone.now()
-            rss.total_articles += news_articles_count
-            rss.save()
+            feed_urls = [entry.link for entry in feed.entries if 'link' in entry]
+            
+            existing_urls = NewsArticle.objects.filter(url__in = feed_urls).values_list('url', flat = True)
+            existing_urls = set(existing_urls)
+
+            try:
+                for idx, entry in enumerate(feed.entries):
+                    try:
+                        if idx % 5 == 0:
+                            rss.refresh_from_db()
+                            if rss.is_active == False:
+                                break
+                    except ObjectDoesNotExist:
+                        break
+
+                    if 'link' not in entry or 'title' not in entry:
+                        continue
+
+                    url = entry.link
+
+                    if url in processed_urls or url in existing_urls:
+                        continue
+                    processed_urls.add(url)
+
+                    title_ru = entry.title
+
+                    try:
+                        topic = pick_up_news_article_topic(title_ru, all_topics)
+
+                        if topic is None:
+                            continue
+
+                        title_ja = translate_title(title_ru)
+
+                        if title_ja is None:
+                            continue
+                    except RateLimitError:
+                        logger.error(f'Rate limit error while translating topics or title')
+                        rss.last_error = 'Rate limit error while translating topics or title'
+                        rss.save()
+                        return
+                    except Exception as e:
+                        logger.error(f'Error translating topics or title: {e}')
+                        rss.last_error = f'Error translating topics or title: {e}'
+                        rss.save()
+                        continue
+
+                    if 'published_parsed' in entry:
+                        published_at = timezone.make_aware(datetime.datetime.fromtimestamp(time.mktime(entry.published_parsed)), datetime.timezone.utc)
+                    else:
+                        published_at = None
+                    
+                    if 'enclosures' in entry and len(entry.enclosures) > 0:
+                        image_url = entry.enclosures[0].href
+                    elif 'media_content' in entry and len(entry.media_content) > 0:
+                        image_url = entry.media_content[0]['url']
+                    elif 'media_thumbnail' in entry and len(entry.media_thumbnail) > 0:
+                        image_url = entry.media_thumbnail[0]['url']
+                    else:
+                        image_url = None
+                    
+                    if image_url is not None:
+                        try:
+                            image = download_image(image_url)
+                        except Exception as e:
+                            logger.error(f'Error downloading image from {image_url}: {e}')
+                            image = None
+                    else:
+                        image = None
+                    
+                    country = rss.country
+
+                    is_title_added = True if title_ru is not None else False
+                    is_title_translated = True if title_ja is not None else False
+                    is_topic_picked = True if topic is not None else False
+
+                    if is_title_added and is_title_translated and is_topic_picked:
+                        is_active = True
+                    else:
+                        is_active = False
+
+                    news_article = NewsArticle(
+                        url = url,
+                        title_ru = title_ru,
+                        title_ja = title_ja,
+                        published_at = published_at,
+                        country = country,
+                        image = image,
+                        rss = rss,
+                        is_active = is_active,
+                        is_title_added = is_title_added,
+                        is_title_translated = is_title_translated,
+                        is_topic_picked = is_topic_picked,
+                    )
+                    pending_articles.append((news_article, topic))
+            finally:
+                if pending_articles:
+                    with transaction.atomic():
+                        for news_article, topic_list in pending_articles:
+                            if not NewsArticle.objects.filter(url = news_article.url).exists():
+                                news_article.save()
+                                news_article.topic.set(topic_list)
+                
+                rss.total_articles = NewsArticle.objects.filter(rss = rss).count()
+                rss.last_fetched_at = timezone.now()
+                rss.save()
             
             time.sleep(2)
         except Exception as e:
@@ -230,7 +219,7 @@ def translate_title(title_ru):
     return None
 
 
-def pick_up_news_article_topic(title):
+def pick_up_news_article_topic(title, topics):
     prompt = f"""
     Classify the following news article title into some of the following topics. 
     - Do not add any extra explanations.
@@ -238,12 +227,11 @@ def pick_up_news_article_topic(title):
     
     Title: {title}
     
-    Topics: {', '.join([topic.name_en for topic in Topic.objects.all()])}
+    Topics: {', '.join([topic.name_en for topic in topics])}
     """
     
     client = Groq(api_key = settings.GROQ_API_KEY)
     models = [settings.GROQ_MODEL_1, settings.GROQ_MODEL_2]
-    topics = Topic.objects.all()
 
     for model in models:
         try:
