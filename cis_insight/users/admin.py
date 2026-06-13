581cc9cb1010629c4e4fd4ef37f87695fd96453b
Inari Goro <qqnq7um9k@gmail.com>
2026-06-13 14:30:54 +0900

    fix: fix admin settings

--- cis_insight/users/admin.py
+++ cis_insight/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import PreUser, User, EmailChange
+from .models import PreUser, User, EmailChange, PasswordReset
 
 admin.site.register(PreUser)
 admin.site.register(User)
 admin.site.register(EmailChange)
+admin.site.register(PasswordReset)
